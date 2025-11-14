9d7b3e59360782c34b9d915d45bc91a0ff8cde67
zyan1226 <zyan1226@163.com>
2025-11-14 15:55:39 +0800

    关闭dts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
             vueDevTools(),
             tailwindcss(),
             Components({
-                dts: true,
+                dts: false,
                 resolvers: []
             })
         ],
