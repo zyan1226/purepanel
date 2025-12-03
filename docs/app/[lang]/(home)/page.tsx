d41dc65e0c91fa5e8dbba8802397c1a72c9085dd
zyan1226 <zyan1226@163.com>
2025-12-03 17:40:40 +0800

    文档更新

--- docs/app/[lang]/(home)/page.tsx
+++ docs/app/[lang]/(home)/page.tsx
@@ -1,30 +1,29 @@
 import Link from 'next/link';
-import {buttonVariants} from "@/components/ui/button";
-import {cn} from "@/lib/cn";
+import {WordRotate} from "@/components/ui/word-rotate";
+import {RainbowButton} from "@/components/ui/rainbow-button";
+import {SparklesText} from "@/components/ui/sparkles-text";
 
 export default function HomePage() {
     return (
         <main className="flex flex-1 flex-col justify-center items-center gap-10">
-            <h1 className="text-8xl font-bold leading-tight">
-                极致轻量的
-                <br/>
-                中后台项目快速启动脚手架</h1>
-            <p className="text-3xl text-fd-muted-foreground">易读、易开发、易维护，优雅高效。</p>
+            <div className="text-5xl font-bold">
+                <WordRotate words={["极致轻量", "开箱即用", "易开发", "优雅高效"]}/>
+                的中后台项目快速启动脚手架
+            </div>
+            <SparklesText className="text-base">
+                <p>一个基于 Spring Boot 3 和 Vue3 的极致轻量的中后台项目快速启动脚手架。</p>
+            </SparklesText>
             <div className="flex flex-row gap-4">
-                <button className={cn(buttonVariants({
-                    color: 'primary'
-                }), 'rounded-full px-4 text-md')}>
+                <RainbowButton size="lg" className="rounded-full px-6 text-md">
                     <Link href='/docs/overview'>
                         快速上手
                     </Link>
-                </button>
-                <button className={cn(buttonVariants({
-                    color: 'outline'
-                }), 'rounded-full px-4 text-md')}>
+                </RainbowButton>
+                <RainbowButton size="lg" variant="outline" className="rounded-full px-6 text-md">
                     <a href='https://github.com/zyan1226/purepanel' target="_blank" rel="noopener noreferrer">
                         GitHub
                     </a>
-                </button>
+                </RainbowButton>
             </div>
         </main>
     );
